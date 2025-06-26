de8bd457f75b8bf319f8e114168ec2ac175f9786
BoulidamAbdellah <133410537+BoulidamAbdellah@users.noreply.github.com>
2025-06-26 16:58:57 +0100

    modifie

--- app.py
+++ app.py
@@ -100,4 +100,5 @@ def chat_voice():
     
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=5001,debug=True,use_reloader=False)
+   port = int(os.environ.get("PORT", 5001))
+   app.run(host='0.0.0.0', port=port,debug=True,use_reloader=False)
